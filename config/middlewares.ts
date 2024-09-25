1b5fbd1ee3bb8086fc0f6bfabe9aec81958da506
Cristian Iriarte <iznogud@kitayori.cl>
2024-09-25 18:18:31 -0300

    middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,10 +2,10 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
+{
     name: 'strapi::cors',
     config: {
-      origin: ['https://strapi.samai.cl'],
+      origin: ['strapi.samai.cl'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
